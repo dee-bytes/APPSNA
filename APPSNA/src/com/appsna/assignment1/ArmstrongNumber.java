6171ddbf6462e19d67b71367b1a048ef1705b6fe
dkuma151 <dkuma151@inlen8520023458.corp.capgemini.com>
2023-07-24 13:56:21 +0530

    Second Commit

---
+++ APPSNA/src/com/appsna/assignment1/ArmstrongNumber.java
@@ -0,0 +1,10 @@
+package com.appsna.assignment1;
+
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+		System.out.println("passed");
+
+	}
+
+}
---
+++ APPSNA/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dkuma151
+ *
+ */
+module APPSNA {
+}
